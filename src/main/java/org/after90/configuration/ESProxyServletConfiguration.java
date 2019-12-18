80125bfc225abcf9ef593c201d62ffdd5edb0d07
Zhao Guangjian <zgj0315@gmail.com>
2019-12-18 19:09:42 +0800

    springboot version is v2.1.6

--- src/main/java/org/after90/configuration/ESProxyServletConfiguration.java
+++ src/main/java/org/after90/configuration/ESProxyServletConfiguration.java
@@ -3,7 +3,6 @@ package org.after90.configuration;
 import lombok.extern.slf4j.Slf4j;
 import org.mitre.dsmiley.httpproxy.ProxyServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
